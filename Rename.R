58f4b0c8fc4e95d5dbb3c64f17f36ee78845de9e
shane denecke <sdenecke@sdenecke_pgrad_gen.biochem.unimelb.edu.au>
2016-08-25 09:07:41 +1000

    a

--- Rename.R
+++ Rename.R
@@ -1,7 +1,7 @@
 
-setwd("C:/Users/Shane/Dropbox/Results/R_Scripts/Renaming")
-rnm=as.character(read.csv("22.1.16 Rename.csv",header=FALSE)$V1)
-setwd("C:/Users/Shane/Desktop/TV6")
+setwd("/home/sdenecke/Dropbox/Results/R_scripts/Renaming")
+rnm=as.character(read.csv("23.8.16.Rename.csv",header=FALSE)$V1)
+setwd("/home/sdenecke/Documents/Temp_Videos_2")
 ob=list.files()[1:length(list.files())]
 
 
